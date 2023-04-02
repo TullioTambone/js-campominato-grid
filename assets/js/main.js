f305dbde67b7ba626e0e3915771776dd4173fa7b
TullioTambone <tulliotambone97@gmail.com>
2023-04-02 16:58:29 +0200

    genero numeri random unici

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,67 +5,53 @@ document.getElementById("play").addEventListener('click', function(){
     let game = document.getElementById("game");
 
     function chooseDifficulty( numCelle, basis ){
-    
+        //setto un array dove salvo i numeri casuali non ripetuti
+        let numeriUnici = [];
+
         for (let i = 0; i < numCelle; i++){
-            let div = creaElementi("div", basis, numCelle);
+            let div = creaElementi("div", basis, numCelle, numeriUnici);
             game.append( div );
+
             div.addEventListener('click', function(){
                 this.classList.toggle( "active" );
             })
+
+            //inserisco numeri casuali
+            div.innerHTML = numeriUnici[i];
         }
     }
     
     switch (difficulty) {
         case "hard":
-            chooseDifficulty(100, "box-hard");
+            chooseDifficulty(100, "box box-hard");
             break;
         
             
         case "medium":
-            chooseDifficulty(81, "box-medium");
+            chooseDifficulty(81, "box box-medium");
             break
         
         case "easy":
-            chooseDifficulty(49, "box-easy");
+            chooseDifficulty(49, "box box-easy");
     }
 });
 
 /*------------------------------- funzioni -----------------------------------------*/
 
-function creaElementi(tag, classi, numCelle){
+function creaElementi(tag, classi, numCelle, numeriUnici){
     let div = document.createElement( tag );
     div.className = classi;
     
-    // let arr = [];
-
-    // for (let i = 1; i <= numCelle; i++) {
-    // arr.push(i);
-    // }
-
-    // for (let i = arr.length - 1; i > 0; i--) {
-    // const j = Math.floor(Math.random() * (i + 1));
-    // [arr[i], arr[j]] = [arr[j], arr[i]];
-    // }
-
-    // for (let i = 0; i < arr.length; i++){
-    //     div.innerHTML = arr[i];
-    // }
-    let numeriUnici = [];
-
     // genera numeri casuali unici
     while (numeriUnici.length < numCelle) {
+        
       let numeroCasuale = Math.floor(Math.random() * numCelle) + 1;
       if (!numeriUnici.includes(numeroCasuale)) {
         numeriUnici.push(numeroCasuale);
       }
     }
-  
-    for (let i = 0; i < numeriUnici.length; i++){
-      div.innerHTML = numeriUnici[i];
-    }
 
     return div
 }
 
-console.log(arr);
 
